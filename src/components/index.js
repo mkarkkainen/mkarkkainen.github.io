e7923cfcd9d1055d45487d0fd64e18c191e6f8a8
Miikka <miikkak@tuta.io>
2021-03-16 22:49:02 -0600

    removed unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export { default as Nav } from "./Nav/Nav";
-export { default as Skills } from "./Skills/Skills";
 export { default as Footer } from "./Footer/Footer";
 export { default as Landing } from "./Landing/Landing";
 export { default as Projects } from "./Projects/Projects";
